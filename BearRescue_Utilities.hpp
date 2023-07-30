f1744529a735eca8672d646bf3fc09af015c7099
SlimRunner <dv.polygon@gmail.com>
2023-07-29 23:00:05 -0700

    Add utilities

---
+++ BearRescue_Utilities.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+int disect(int prove, int limits[], int size) {
+  int section;
+  for (section = 0; section < size && prove <= limits[size]; ++section) {
+
+  }
+  return section;
+}
