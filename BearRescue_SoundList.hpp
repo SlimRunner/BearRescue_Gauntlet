c955537afd5a8c70c374983db89d889a8876b718
SlimRunner <dv.polygon@gmail.com>
2023-07-30 03:37:01 -0700

    added melodies

---
+++ BearRescue_SoundList.hpp
@@ -0,0 +1,6 @@
+#include "BearRescue_Note.hpp"
+
+// Mario life up sound (quarter is 1 at 180 bpm)
+const Note sound_intro[7] = {3211266, 12386305, 12582913, 13172737, 12910593, 13041665, 13369345};
+
+const Note sound_alarm[6] = {12976129, 12976129, 4587521, 12976129, 12976129, 4587521};
